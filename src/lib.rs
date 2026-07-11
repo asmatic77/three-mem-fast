43733c429e05ecaf2f85c15b600d6a78972f2ef2
Sergio Vera <sergio.vera.h@gmail.com>
2026-07-11 19:56:16 +0200

    Simplified code and fixed some logic flaws

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,9 @@
-use std::fs::File;
 use std::io::BufReader;
 use std::path::Path;
 use std::result::Result;
+use std::{borrow::Cow, fs::File};
 
-use quick_xml::events::Event;
+use quick_xml::events::{BytesStart, Event};
 use zip::{ZipArchive, result::ZipError};
 
 use crate::Error::{InvalidContentType, MissingFile, MissingRootPart, XmlFormat};
@@ -27,7 +27,7 @@ pub enum Error {
     #[error("Invalid content type for file {0}")]
     InvalidContentType(String),
 }
-
+#[derive(Debug)]
 pub struct ThreeMFContainer {
     #[allow(dead_code)]
     archive: zip::ZipArchive<BufReader<File>>,
@@ -59,17 +59,11 @@ pub fn open(path: &Path) -> Result<ThreeMFContainer, Error> {
             Ok(Event::Empty(e)) | Ok(Event::Start(e)) => {
                 //println!("Starting tag event with tag {:?}", e.name());
                 if e.name().as_ref() == RELATIONSHIP_TAG {
-                    let is_node = e
-                        .attributes()
-                        .flatten()
-                        .find(|attr| attr.key.as_ref() == TYPE_ATTR)
-                        .is_some_and(|attr| attr.value.as_ref() == MODEL_RELATIONSHIP_TYPE);
+                    let is_node = find_attr_value(&e, TYPE_ATTR)
+                        .is_some_and(|v| v.as_ref() == MODEL_RELATIONSHIP_TYPE);
                     if is_node {
-                        let target = e
-                            .attributes()
-                            .flatten()
-                            .find(|attr| attr.key.as_ref() == TARGET_ATTR)
-                            .map(|attr| attr.value.into_owned())
+                        let target = find_attr_value(&e, TARGET_ATTR)
+                            .map(Cow::into_owned)
                             .ok_or(MissingRootPart)?;
                         target_path = String::from_utf8(target)?;
                         //println!("The root part path is {}", target_path);
@@ -83,10 +77,7 @@ pub fn open(path: &Path) -> Result<ThreeMFContainer, Error> {
         buf.clear();
     }
     drop(xml_reader);
-
-    if !validate_part_type(&mut zip_archive, &target_path)? {
-        return Err(InvalidContentType(target_path));
-    }
+    if target_path.is_empty() { return Err(MissingRootPart); }
 
     Ok(ThreeMFContainer {
         archive: zip_archive,
@@ -97,7 +88,7 @@ pub fn open(path: &Path) -> Result<ThreeMFContainer, Error> {
 fn validate_part_type(
     zipfile: &mut ZipArchive<BufReader<File>>,
     root_part_path: &str,
-) -> Result<bool, Error> {
+) -> Result<(), Error> {
     const CONTENT_TYPES_ENTRY: &str = "[Content_Types].xml";
     const DEFAULT_TAG: &[u8] = b"Default";
     const EXTENSION_ATTR: &[u8] = b"Extension";
@@ -108,31 +99,30 @@ fn validate_part_type(
         Ok(content_types_file) => content_types_file,
         Err(zip_error) => return Err(MissingFile(zip_error, CONTENT_TYPES_ENTRY.to_string())),
     };
+
     let mut xml_reader = quick_xml::Reader::from_reader(BufReader::new(content_types_file));
     let mut buf = Vec::new();
     let extension = Path::new(&root_part_path)
         .extension()
         .and_then(|ext| ext.to_str())
         .unwrap_or_default();
-    let mut valid_content_type = false;
     loop {
         match xml_reader.read_event_into(&mut buf) {
             Ok(Event::Empty(e)) | Ok(Event::Start(e)) => {
                 if e.name().as_ref() == DEFAULT_TAG {
-                    let ext_matches = e.attributes().flatten().any(|attr| {
-                        (attr.key.as_ref() == EXTENSION_ATTR)
-                            && (attr.value.as_ref() == extension.as_bytes())
-                    });
+                    let ext_matches = find_attr_value(&e, EXTENSION_ATTR)
+                        .is_some_and(|v| v.as_ref() == extension.as_bytes());
 
-                    if ext_matches
-                        && let Some(content_type_attr) = e
-                            .attributes()
-                            .flatten()
-                            .find(|attr| attr.key.as_ref() == CONTENT_TYPE_ATTR)
-                    {
-                        valid_content_type = content_type_attr.value.as_ref() == MODEL_CONTENT_TYPE;
-                        println!("The 3mf root_part path has the correct content type");
-                        return Ok(valid_content_type);
+                    if ext_matches {
+                        let content_type_marches = find_attr_value(&e, CONTENT_TYPE_ATTR)
+                            .is_some_and(|v| v.as_ref() == MODEL_CONTENT_TYPE);
+                        if content_type_marches {
+                            println!("The 3mf root_part path has the correct content type");
+                            return Ok(());
+                        } else {
+                            return Err(InvalidContentType(root_part_path.to_string()));
+                        }
+                        
                     }
                 }
             }
@@ -142,7 +132,14 @@ fn validate_part_type(
         }
         buf.clear();
     }
-    Ok(valid_content_type)
+    Err(InvalidContentType(root_part_path.to_string()))
+}
+
+fn find_attr_value<'a>(e: &'a BytesStart, key: &[u8]) -> Option<Cow<'a, [u8]>> {
+    e.attributes()
+        .flatten()
+        .find(|attr| attr.key.as_ref() == key)
+        .map(|attr| attr.value)
 }
 
 #[cfg(test)]
@@ -154,4 +151,5 @@ mod tests {
         let container = open(Path::new("fixtures/core/box.3mf")).unwrap();
         assert_eq!(container.root_part_path, "/3D/3dmodel.model");
     }
+
 }
