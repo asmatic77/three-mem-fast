1a2b7b1d441bdfc820dc5828f84c08f1fd80b679
Sergio Vera <sergio.vera.h@gmail.com>
2026-07-19 19:36:25 +0200

    added parsing and storing of geometry via Mesh Sink.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::io::BufReader;
 use std::path::Path;
 use std::result::Result;
@@ -6,7 +7,9 @@ use std::{borrow::Cow, fs::File};
 use quick_xml::events::{BytesStart, Event};
 use zip::{ZipArchive, result::ZipError};
 
-use crate::Error::{InvalidContentType, MissingAttribute, MissingFile, MissingRootPart, XmlFormat};
+use crate::Error::{
+    InvalidContentType, MissingAttribute, MissingFile, MissingRootPart, NoOpenObject, XmlFormat,
+};
 
 #[derive(thiserror::Error, Debug)]
 pub enum Error {
@@ -28,11 +31,15 @@ pub enum Error {
     InvalidContentType(String),
     #[error("Missing attribute {0} in element {1}")]
     MissingAttribute(String, String),
+    #[error("Geometry found outside object")]
+    NoOpenObject,
+    #[error("Out of memory: could not reserve memory for data")]
+    OutOfMemory(#[from] std::collections::TryReserveError),
 }
 
 #[derive(Debug)]
 #[allow(dead_code)]
-pub struct ThreeMFContainer {
+pub struct Parser3mf {
     archive: zip::ZipArchive<BufReader<File>>,
     root_part_path: String,
     geometry_stats: GeometryStatistics,
@@ -47,23 +54,121 @@ pub struct GeometryStatistics {
     pub build_items: usize,
 }
 
-#[allow(dead_code)]
 #[derive(Debug, Default)]
+#[repr(C)]
 pub struct Vertex {
     x: f32,
     y: f32,
     z: f32,
 }
 
-#[allow(dead_code)]
 #[derive(Debug, Default)]
+#[repr(C)]
 pub struct Triangle {
     v1: u32,
     v2: u32,
     v3: u32,
 }
 
-pub fn open(path: &Path) -> Result<ThreeMFContainer, Error> {
+pub type Transform = [f32; 12];
+
+pub struct Mesh {
+    pub vertices: Vec<Vertex>,
+    pub triangles: Vec<Triangle>,
+}
+
+pub struct Object {
+    pub id: u32,
+    pub mesh: Mesh,
+    pub name: String,
+}
+
+pub struct Item {
+    pub object_id: u32,
+    pub transform: Option<Transform>, // None means identity Transform
+    pub partname: String,
+}
+
+pub struct Scene3mf {
+    pub objects: HashMap<u32, Object>,
+    pub build_items: Vec<Item>,
+}
+
+#[derive(Default)]
+pub struct Scene3mfBuilder {
+    objects: HashMap<u32, Object>,
+    build_items: Vec<Item>,
+    current: Option<Object>,
+}
+
+pub trait MeshSink {
+    fn begin_object(&mut self, id: u32) -> Result<(), Error>;
+    fn end_object(&mut self) -> Result<(), Error>;
+    fn vertex(&mut self, x: f32, y: f32, z: f32) -> Result<(), Error>;
+    fn triangle(&mut self, v1: u32, v2: u32, v3: u32) -> Result<(), Error>;
+    fn build_item(&mut self, object_id: u32, transform: Option<Transform>) -> Result<(), Error>;
+}
+
+impl MeshSink for Scene3mfBuilder {
+    fn begin_object(&mut self, id: u32) -> Result<(), Error> {
+        if self.current.is_some() {
+            return Err(NoOpenObject);
+        }
+        const INITIAL_VERTEX_RESERVE: usize = 1024;
+        const INITIAL_TRIANGLE_RESERVE: usize = INITIAL_VERTEX_RESERVE*2;
+        let mut vertices = Vec::new();
+        vertices.try_reserve(INITIAL_VERTEX_RESERVE)?;
+        let mut triangles = Vec::new();
+        triangles.try_reserve(INITIAL_TRIANGLE_RESERVE)?;
+        self.current = Some(Object {
+            id,
+            mesh: Mesh {
+                vertices,
+                triangles,
+            },
+            name: String::new(),
+        });
+        Ok(())
+    }
+    fn end_object(&mut self) -> Result<(), Error> {
+        let obj = self.current.take().ok_or(NoOpenObject)?;
+        self.objects.insert(obj.id, obj);
+        // TODO: evaluate call to shrink_to_fit in the triangles and vertices
+        Ok(())
+    }
+
+    fn build_item(&mut self, object_id: u32, transform: Option<Transform>) -> Result<(), Error> {
+        let item = Item {
+            object_id,
+            transform,
+            partname: String::new(),
+        };
+        self.build_items.push(item);
+        Ok(())
+    }
+
+    fn vertex(&mut self, x: f32, y: f32, z: f32) -> Result<(), Error> {
+        let obj = self.current.as_mut().ok_or(Error::NoOpenObject)?;
+        obj.mesh.vertices.push(Vertex { x, y, z });
+        Ok(())
+    }
+    fn triangle(&mut self, v1: u32, v2: u32, v3: u32) -> Result<(), Error> {
+        let obj = self.current.as_mut().ok_or(Error::NoOpenObject)?;
+        obj.mesh.triangles.push(Triangle { v1, v2, v3 });
+        Ok(())
+    }
+}
+
+impl Scene3mfBuilder {
+    pub fn into_scene(self) -> Scene3mf {
+        Scene3mf {
+            objects: self.objects,
+            build_items: self.build_items,
+        }
+    }
+}
+
+pub fn open(path: &Path) -> Result<Parser3mf, Error> {
     const RELS_ENTRY: &str = "_rels/.rels";
     const RELATIONSHIP_TAG: &[u8] = b"Relationship";
     const TYPE_ATTR: &[u8] = b"Type";
@@ -110,7 +215,7 @@ pub fn open(path: &Path) -> Result<ThreeMFContainer, Error> {
     }
     validate_part_type(&mut zip_archive, &target_path)?;
 
-    Ok(ThreeMFContainer {
+    Ok(Parser3mf {
         archive: zip_archive,
         root_part_path: target_path,
         geometry_stats: GeometryStatistics::default(),
@@ -174,8 +279,8 @@ fn find_attr_value<'a>(e: &'a BytesStart, key: &[u8]) -> Option<Cow<'a, [u8]>> {
         .map(|attr| attr.value)
 }
 
-impl ThreeMFContainer {
-    pub fn parse_root_part(&mut self) -> Result<(), Error> {
+impl Parser3mf {
+    pub fn parse_root_part(&mut self, sink: &mut impl MeshSink) -> Result<(), Error> {
         let root_file_path = self
             .root_part_path
             .strip_prefix("/")
@@ -200,30 +305,32 @@ impl ThreeMFContainer {
         const ITEM_TAG: &[u8] = b"item";
 
         let mut stats = GeometryStatistics::default();
-        let mut _current_tag: Option<Vec<u8>>; // TODO maybe we dont need to track this?
         loop {
             match xml_reader.read_event_into(&mut buf) {
                 Ok(Event::Empty(e)) | Ok(Event::Start(e)) => {
-                    _current_tag = Some(Vec::from(e.name().as_ref()));
                     match e.name().as_ref() {
                         MODEL_TAG => (),
                         RESOURCES_TAG => (),
-                        OBJECT_TAG => ThreeMFContainer::parse_object(&e, &mut stats),
+                        OBJECT_TAG => Parser3mf::parse_object(&e, sink, &mut stats)?,
                         MESH_TAG => (),
                         VERTICES_TAG => (),
                         VERTEX_TAG => {
-                            ThreeMFContainer::parse_vertex(&e, &mut stats)?;
+                            Parser3mf::parse_vertex(&e, sink, &mut stats)?;
                         }
                         TRIANGLES_TAG => (),
                         TRIANGLE_TAG => {
-                            ThreeMFContainer::parse_triangle(&e, &mut stats)?;
+                            Parser3mf::parse_triangle(&e, sink, &mut stats)?;
                         }
                         BUILD_TAG => (),
-                        ITEM_TAG => ThreeMFContainer::parse_item(&e, &mut stats),
-                        _ => (),
+                        ITEM_TAG => Parser3mf::parse_item(&e, sink, &mut stats)?,
+                        _ => (), // TODO: components!
                     }
                 }
-                Ok(Event::End(_e)) => _current_tag = None,
+                Ok(Event::End(e)) => {
+                    if e.name().as_ref() == OBJECT_TAG {
+                        sink.end_object()?
+                    }
+                }
                 Err(e) => return Err(XmlFormat(e)),
                 Ok(Event::Eof) => break,
                 _ => (),
@@ -235,14 +342,26 @@ impl ThreeMFContainer {
         Ok(())
     }
 
-    fn parse_object(_object_element: &BytesStart, stats: &mut GeometryStatistics) {
+    fn parse_object(
+        object_element: &BytesStart,
+        sink: &mut impl MeshSink,
+        stats: &mut GeometryStatistics,
+    ) -> Result<(), Error> {
+        let id = find_attr_value(object_element, b"id")
+            .as_deref()
+            .and_then(|bytes| std::str::from_utf8(bytes).ok())
+            .and_then(|s| s.parse::<u32>().ok())
+            .ok_or(MissingAttribute("id".to_string(), "object".to_string()))?;
+        sink.begin_object(id)?;
         stats.object_count += 1;
+        Ok(())
     }
 
     fn parse_vertex(
         vertex_element: &BytesStart,
+        sink: &mut impl MeshSink,
         stats: &mut GeometryStatistics,
-    ) -> Result<Vertex, Error> {
+    ) -> Result<(), Error> {
         let x = find_attr_value(vertex_element, b"x")
             .as_deref()
             .and_then(|bytes| std::str::from_utf8(bytes).ok())
@@ -261,13 +380,15 @@ impl ThreeMFContainer {
             .and_then(|s| s.parse::<f32>().ok())
             .ok_or(MissingAttribute("z".to_string(), "vertex".to_string()))?;
         stats.vertex_count += 1;
-        Ok(Vertex { x, y, z })
+        sink.vertex(x, y, z)?;
+        Ok(())
     }
 
     fn parse_triangle(
         triangle_element: &BytesStart,
+        sink: &mut impl MeshSink,
         stats: &mut GeometryStatistics,
-    ) -> Result<Triangle, Error> {
+    ) -> Result<(), Error> {
         let v1 = find_attr_value(triangle_element, b"v1")
             .as_deref()
             .and_then(|bytes| std::str::from_utf8(bytes).ok())
@@ -287,32 +408,71 @@ impl ThreeMFContainer {
             .ok_or(MissingAttribute("v3".to_string(), "triangle".to_string()))?;
 
         stats.triangle_count += 1;
-
-        Ok(Triangle { v1, v2, v3 })
+        sink.triangle(v1, v2, v3)?;
+        Ok(())
     }
 
-    fn parse_item(_item_element: &BytesStart, stats: &mut GeometryStatistics) {
+    fn parse_item(
+        item_element: &BytesStart,
+        sink: &mut impl MeshSink,
+        stats: &mut GeometryStatistics,
+    ) -> Result<(), Error> {
+        let object_id = find_attr_value(item_element, b"objectid")
+            .as_deref()
+            .and_then(|bytes| std::str::from_utf8(bytes).ok())
+            .and_then(|s| s.parse::<u32>().ok())
+            .ok_or(MissingAttribute("objectid".to_string(), "item".to_string()))?;
         stats.build_items += 1;
+        sink.build_item(object_id, None)?;
+        Ok(())
     }
 }
 
 #[cfg(test)]
 mod tests {
+
     use super::*;
 
     #[test]
     fn open_finds_root_part_relationship() {
-        let container = open(Path::new("fixtures/core/box.3mf")).unwrap();
-        assert_eq!(container.root_part_path, "/3D/3dmodel.model");
+        let parser = open(Path::new("fixtures/core/box.3mf")).unwrap();
+        assert_eq!(parser.root_part_path, "/3D/3dmodel.model");
     }
 
     #[test]
     fn open_and_parses_geometry() {
-        let mut container = open(Path::new("fixtures/core/box.3mf")).unwrap();
-        container.parse_root_part().unwrap();
-        assert_eq!(container.geometry_stats.object_count, 1);
-        assert_eq!(container.geometry_stats.build_items, 1);
-        assert_eq!(container.geometry_stats.vertex_count, 8);
-        assert_eq!(container.geometry_stats.triangle_count, 12);
+        let mut parser = open(Path::new("fixtures/core/box.3mf")).unwrap();
+        let mut builder = Scene3mfBuilder::default();
+        parser.parse_root_part(&mut builder).unwrap();
+        assert_eq!(parser.geometry_stats.object_count, 1);
+        assert_eq!(parser.geometry_stats.build_items, 1);
+        assert_eq!(parser.geometry_stats.vertex_count, 8);
+        assert_eq!(parser.geometry_stats.triangle_count, 12);
+    }
+
+    #[test]
+    fn opens_parses_and_stores_geometry() {
+        let mut parser = open(Path::new("fixtures/core/box.3mf")).unwrap();
+        let mut builder: Scene3mfBuilder = Scene3mfBuilder::default();
+        parser.parse_root_part(&mut builder).unwrap();
+
+        assert_eq!(builder.objects.len(), 1);
+        assert_eq!(builder.build_items.len(), 1);
+        let obj = builder.objects.get(&1).expect("object id 1");
+        assert_eq!(obj.mesh.vertices.len(), 8);
+        assert_eq!(obj.mesh.triangles.len(), 12);
+    }
+
+    #[test]
+    fn opens_parses_and_returns_scene() {
+        let mut parser = open(Path::new("fixtures/core/box.3mf")).unwrap();
+        let mut builder: Scene3mfBuilder = Scene3mfBuilder::default();
+        parser.parse_root_part(&mut builder).unwrap();
+        let scene = builder.into_scene();
+        assert_eq!(scene.objects.len(), 1);
+        assert_eq!(scene.build_items.len(), 1);
+        let obj = scene.objects.get(&1).expect("object id 1");
+        assert_eq!(obj.mesh.vertices.len(), 8);
+        assert_eq!(obj.mesh.triangles.len(), 12);
     }
 }
